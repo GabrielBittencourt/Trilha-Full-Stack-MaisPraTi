233529240559d01706e15f36ebcdec04fd76f7d3
GabrielBittencourt <gdsbittencourt@inf.ufpel.edu.br>
2024-04-17 17:22:28 -0300

    Update exercicio3.js

--- modulo01/atividade01/exercicio3.js
+++ modulo01/atividade01/exercicio3.js
@@ -3,6 +3,7 @@ const prompt = require('prompt-sync')();
 let numero1 = parseInt(prompt('Digite o primeiro número: '))
 let numero2 = parseInt(prompt('Digite o segundo número: '))
 let numero3 = parseInt(prompt('Digite o terceiro número: '))
+let numero4 = parseInt(prompt('Digite o quarto número: '))
 
 numero4 = numero1 + numero2 + numero3
 numero1 += 25
